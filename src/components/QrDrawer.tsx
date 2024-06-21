ce308f89921de9bd9bbedd14bfd56a63b3fb1666
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-22 06:53:10 +0800

    refactor: add drawer trigger and remove isOpen props

--- src/components/QrDrawer.tsx
+++ src/components/QrDrawer.tsx
@@ -1,43 +1,44 @@
-import QRCode from 'react-qr-code';
-import { v4 as uuidv4 } from 'uuid';
-
+import QRCode from "react-qr-code";
+import { v4 as uuidv4 } from "uuid";
+import { UserPlusIcon } from "@heroicons/react/24/solid";
 import {
-	Drawer,
-	DrawerContent,
-	DrawerDescription,
-	DrawerFooter,
-	DrawerHeader,
-	DrawerTitle,
-} from './ui/drawer';
-import { Button } from './ui/button';
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
+import { Button } from "./ui/button";
 
-type Props = {
-	isOpen: boolean;
-};
+export const QrDrawer = () => {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <button>
+          <UserPlusIcon className="h-6 w-6" />
+        </button>
+      </DrawerTrigger>
+      <DrawerContent className="container flex items-center">
+        <div>
+          <DrawerHeader>
+            <div className="flex flex-col items-start gap-y-1">
+              <DrawerTitle className="text-base text-primary">
+                Join me
+              </DrawerTitle>
+              <DrawerDescription className="mb-4">
+                Scan this QR code to start adding items.
+              </DrawerDescription>
+            </div>
 
-export const QrDrawer = ({ isOpen }: Props) => {
-	return (
-		<Drawer open={isOpen}>
-			<DrawerContent className='flex items-center'>
-				<div>
-					<DrawerHeader>
-						<div className='flex flex-col items-start gap-y-1'>
-							<DrawerTitle className='text-primary text-base'>Join me</DrawerTitle>
-							<DrawerDescription className='mb-4'>
-								Scan this QR code to start adding items.
-							</DrawerDescription>
-						</div>
-
-						<QRCode
-							value={uuidv4()}
-							className='w-full'
-						/>
-					</DrawerHeader>
-					<DrawerFooter>
-						<Button className='w-full'>Start Ordering</Button>
-					</DrawerFooter>
-				</div>
-			</DrawerContent>
-		</Drawer>
-	);
+            <QRCode value={uuidv4()} className="w-full" />
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button className="w-full">Start Ordering</Button>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
 };
