0079561b9808c611449d2d01a307faff29d80bec
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-22 07:05:28 +0800

    refactor: remove searchBox component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/header/Navbar";
 
-import SearchBox from "@/components/ui/searchBox";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
         <Navbar />
- <SearchBox/>
         {children}
       </body>
     </html>
