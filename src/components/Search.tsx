1d852c58cecbba01761143ce83cb9cb51326c832
Fidelito Pardillo <63651138+fidsolit@users.noreply.github.com>
2024-06-21 09:27:12 +0800

    changing focus from blue to orange

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,7 +7,7 @@ const Search = () => {
         <input
           type="text"
           placeholder="Search here"
-          className="pl-4 pr-10 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          className="pl-4 pr-10 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-transparent"
         />
         <div className="absolute inset-y-0 right-0 flex items-center pr-3">
           <svg
