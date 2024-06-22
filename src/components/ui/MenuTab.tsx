e0b04561968a31d56700a95a1f40fc9fdc47da51
Kamnida1992 <vincentsanpedro2015@gmail.com>
2024-06-22 17:03:31 +0800

    Refactoring: Icons on menu tab component

--- src/components/ui/MenuTab.tsx
+++ src/components/ui/MenuTab.tsx
@@ -2,8 +2,8 @@
 import React, { useRef } from "react";
 import categoryItems from "../../../api/categoryItems";
 import Image from "next/image";
-
 const MenuTab = () => {
+  
   const buttonRef = useRef<(HTMLButtonElement | null)[]>([]);
 
   const handleFocus = (e: React.FocusEvent<HTMLButtonElement>) => {
@@ -22,22 +22,22 @@ const MenuTab = () => {
         {categoryItems.map((item, index) => (
           <div
             key={index}
-            className="flex flex-col items-center justify-center gap-x-5"
+            className="flex items-center justify-center gap-x-5"
           >
             <button
               ref={(e: any) => (buttonRef.current[index] = e)}
               onFocus={handleFocus}
-              className="flex w-[100px] flex-col items-center rounded-sm py-3 shadow-md hover:bg-zinc-200 focus:bg-orange-400 focus:text-white"
+              className="flex flex-row items-center rounded-sm py-3 px-2 space-x-1 shadow-md
+              justify-center text-gray-500 font-semibold hover:bg-zinc-200 focus:bg-orange-400 focus:text-white"
             >
               <span>
                 {item.img && (
-                  <div className="relative aspect-square h-10 w-10">
-                    <Image
-                      className="rounded-full focus:bg-orange-400"
-                      fill
-                      src={item.img}
-                      alt={item.title}
-                    />
+                  <div>
+                    {typeof item.img === 'string' ? (
+                   <img src={item.img} alt={item.title} />
+                  ) : (
+                   item.img
+                  )}
                   </div>
                 )}
               </span>
