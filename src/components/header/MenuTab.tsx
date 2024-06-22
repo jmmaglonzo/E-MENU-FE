d92477a9f74d17c73ab4eb3ef449e0f72c4e042f
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-22 20:03:29 +0800

    chore: move components to header folder

---
+++ src/components/header/MenuTab.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React, { useRef } from "react";
+import categoryItems from "@/utils/categoryItems";
+const MenuTab = () => {
+  const buttonRef = useRef<(HTMLButtonElement | null)[]>([]);
+
+  const handleFocus = (e: React.FocusEvent<HTMLButtonElement>) => {
+    if (e.target) {
+      e.target.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "center",
+      });
+    }
+  };
+  return (
+    <div className="container mt-2 flex items-center gap-2 text-xs">
+      {categoryItems.slice(0, 1).map((item, index) => (
+        <button
+          key={index}
+          ref={(e: any) => (buttonRef.current[index] = e)}
+          className="flex items-center justify-center space-x-1 rounded-sm px-2 py-3 font-semibold text-gray-500 shadow-md hover:bg-zinc-200 focus:bg-orange-400 focus:text-white"
+        >
+          {item.title}
+        </button>
+      ))}
+      <div className="flex justify-between gap-x-2 overflow-x-scroll whitespace-nowrap py-2 no-scrollbar">
+        {/* rendering tab item title */}
+
+        {categoryItems.slice(1).map((item, index) => (
+          <div key={index} className="flex items-center justify-center gap-x-5">
+            <button
+              ref={(e: any) => (buttonRef.current[index] = e)}
+              onFocus={handleFocus}
+              className="flex items-center justify-center space-x-1 rounded-sm px-2 py-3 font-semibold text-gray-500 shadow-md hover:bg-zinc-200 focus:bg-orange-400 focus:text-white"
+            >
+              {item.icon}
+              <span className="truncate">{item.title}</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default MenuTab;
---
+++ src/components/header/searchBox.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useState } from "react";
+
+const SearchBox = () => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Implement search functionality here
+    console.log("Searching for:", query);
+  };
+
+  return (
+    <div className="container mx-auto mt-6 flex w-[90%] items-center rounded-md border-2 px-4 focus-within:border-orange-500 md:w-[400px]">
+      <input
+        type="text"
+        className="w-full flex-grow py-2 outline-none focus:border-orange-500"
+        placeholder="Search here"
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>
+        <svg
+          className="h-5 w-5 text-gray-500"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-4.35-4.35m1.9-5.15a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBox;
