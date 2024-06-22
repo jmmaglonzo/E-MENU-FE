8a24b5c4878fea93d20eb8097651ac362dcab466
cybr2 <ydoowbernisca2@gmail.com>
2024-06-23 02:55:14 +0800

    productCard final output

--- src/stories/ProjectCard.stories.ts
+++ src/stories/ProjectCard.stories.ts
@@ -1,45 +1,34 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { ProductCard } from '@/components/Card/ProductCard'
-import menu1 from '@/assets/menu1.jpg';
+import type { Meta, StoryObj } from "@storybook/react";
+import { ProductCard } from "@/components/Card/ProductCard";
+import menu1 from "@/assets/menu1.jpg";
 
-const dummyData =
-{
-    product_id: 'qwewe',
-    name: 'adobo',
-    description: 'blah blah blah',
-    price: 100.00,
-    image: menu1,
-    time: {
-        "best_case": '00:15:00',
-        'worst_case': '00:40:00',
-    },
-    rating: 5,
-}
+const dummyData = {
+  product_id: "qwewe",
+  name: "Pastry and Boiled Egg on Plate",
+  description:
+    "Enjoy a freshly baked pastry, golden and flaky, paired perfectly with a hearty boiled egg.",
+  price: 200.0,
+  image: menu1,
+  time: {
+    best_case: "00:01:00",
+    worst_case: "00:10:00",
+  },
+  rating: 5,
+};
 
 const meta: Meta<typeof ProductCard> = {
-    title: "ProductCard",
-    component: ProductCard,
-    parameters: {
-        layout: 'centered',
-      },
-}
+  title: "ProductCard",
+  component: ProductCard,
+  parameters: {
+    layout: "centered",
+  },
+};
 
 export default meta;
 type Story = StoryObj<typeof ProductCard>;
 
-
 export const Default: Story = {
-    args: {
-        data: dummyData
-    },
-  };
-
-
-
-
-
-
-
-
-
-  
+  args: {
+    data: dummyData,
+  },
+};
