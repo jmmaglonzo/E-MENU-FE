8c6958d6053cee3f57fa8c9bb87d3b5d0b1dfd46
Kamnida1992 <vincentsanpedro2015@gmail.com>
2024-06-22 09:36:13 +0800

    Adding ScrollIntoView in menu tab

--- api/categoryItems.ts
+++ api/categoryItems.ts
@@ -1,9 +1,15 @@
 const categoryItems = [
-    { title: 'All'},
-    { title: 'Popular'},
-    { title: 'Appetizers'},
-    { title: 'Main Course'},
-    { title: 'Side Dishes'},
+    { title: 'All ', img: '/pizza.png'},
+    { title: 'Popular' , img: '/pizza.png'},
+    { title: 'Appetizers' , img: '/pizza.png'},
+    { title: 'Main Course' , img: '/pizza.png'},
+    { title: 'Side Dishes' , img: '/pizza.png'},
+    { title: 'Desserts' , img: '/pizza.png'},
+    { title: 'Cocktails' , img: '/pizza.png'},
+    { title: 'Sweets' , img: '/pizza.png'},
+
+     { title: 'Sea Food' , img: '/pizza.png'},
+     { title: 'Pizza' , img: '/pizza.png'},
     
 ]
 export default categoryItems
--- src/components/category_tab/MenuTab.tsx
+++ src/components/category_tab/MenuTab.tsx
@@ -1,18 +1,39 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
 import categoryItems from "../../../api/categoryItems"
 
 const MenuTab = () => {
+  const buttonRef = useRef<(HTMLButtonElement | null)[]>([]);
+
+  const handleFocus = (e: React.FocusEvent<HTMLButtonElement>) => {
+    if(e.target){
+      e.target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center',
+      })
+    }
+  }
   return (
     <div className='flex justify-center text-xs mx-2 mt-10 p-2  '>
         <div className='flex justify-between overflow-x-scroll 
-        no-scrollbar whitespace-nowrap'>
-        {categoryItems.map((item, index) => (
+        no-scrollbar whitespace-nowrap gap-x-2'>
+          {/* rendering tab item title */}
+        {categoryItems.map((item, index) => (     
+          <div key={index} className='flex flex-col justify-center items-center space-x-2'>
             <button
-             key={index}
-             className='hover:bg-zinc-200 rounded-sm p-2 py-3
-             focus:bg-orange-400 focus:text-white'>
-                {item.title}
+             ref={(e:any) =>(buttonRef.current[index] = e)}
+             onFocus={handleFocus}
+
+             className='hover:bg-zinc-100 rounded-sm flex flex-col items-center w-[80px] py-3
+             focus:bg-orange-400 focus:text-white shadow-2xl'>
+                {/* <span>{item.img && <img className='w-10 h-10 rounded-full bg-slate-100 focus:bg-orange-400 object-fill'
+                 src={item.img} 
+                 alt={item.title} />}</span> */}
+
+                <span className='truncate'>{item.title}</span>
             </button>
+            </div>
         ))}
         </div>
     </div>
