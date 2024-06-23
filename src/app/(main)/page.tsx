135d07a074bad968e7c324faed4725b8d57ee87e
joben <jobenapao10@gmail.com>
2024-06-23 23:58:58 +0800

    feat: order button drawer.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from '@/components/ui/button';
+import { OrderDrawer } from "@/components/OrderDrawer";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="container flex min-h-screen flex-col items-center">
+      <OrderDrawer />
+    </main>
+  );
 }
