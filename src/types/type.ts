3131d7d0dd43d498906544b4381f92e6edf3e526
cybr2 <ydoowbernisca2@gmail.com>
2024-06-23 15:42:49 +0800

    menu component

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,14 +1,16 @@
-import { StaticImageData } from 'next/image'
+import { StaticImageData } from "next/image";
 
 export type CardObject = {
-    product_id: string;
-    name: string;
-    description: string;
-    price: number;
-    image: StaticImageData;
-    time: {
-        best_case: string;
-        worst_case: string;
-    };
-    rating: number;
+  product_id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: StaticImageData;
+  time: {
+    best_case: string;
+    worst_case: string;
+  };
+  rating: number;
 };
+
+export type MenuArray = CardObject[];
