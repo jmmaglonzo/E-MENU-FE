866899420d59354dc2ea3aa4aa7e2469c573ee96
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-23 16:18:02 +0800

    chore: move files to route group folder

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Navbar from "@/components/header/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
-        <Navbar />
+        <header>
+          <Navbar />
+        </header>
+
         {children}
       </body>
     </html>
