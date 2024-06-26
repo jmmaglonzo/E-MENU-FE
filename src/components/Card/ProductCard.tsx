8b6ad87605e5ee3d0b89216212a17fd06b9a6dfa
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-26 23:50:37 +0800

    refactor: change queryFn to readable for more readable code

--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -1,15 +1,10 @@
 "use client";
 import api from "@/utils/axios";
-import getProducts from "@/utils/getMenuFn";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 const ProductCard = () => {
   const { data } = useQuery({
     queryKey: ["users"],
-    queryFn: () =>
-      axios
-        .get("https://e-menu-be.onrender.com/api/products")
-        .then((res) => res.data),
+    queryFn: () => api.get("users").then((res) => res.data),
   });
 
   console.log(data);
