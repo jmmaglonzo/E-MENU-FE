36f7b7f6c18086f094599658c7f3da3e7823ae76
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-26 22:50:04 +0800

    feat: add custom react query provider

---
+++ src/utils/Provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // set default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 4 * 1000,
+            refetchInterval: 4 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
