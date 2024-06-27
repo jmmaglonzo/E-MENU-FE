690acdfdd3a9cc0789751b2396c56c763018110e
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-27 09:37:15 +0800

    chore: change name of the provider component and ad querydevtools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
-import ReactQueryProvider from "@/utils/Provider";
+import QueryProvider from "@/utils/Provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
--- src/utils/Provider.tsx
+++ src/utils/Provider.tsx
@@ -2,13 +2,23 @@
 import { ReactNode } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-
-const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const QueryProvider = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: { retry: 5, retryDelay: 1000 },
+        },
+      }),
+  );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
-export default ReactQueryProvider;
+export default QueryProvider;
