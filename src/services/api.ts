25e2a7d04b02fad339dc2ef4ac6aa8164941f9b7
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-27 19:10:18 +0800

    refactor: add  types for product

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
+import { ProductType } from "@/types/productCard";
 import api from "./axios";
 
 export const getProducts = async () => {
-  const { data } = await api.get("products");
+  const { data } = await api.get<ProductType[]>("products");
   return data;
 };
