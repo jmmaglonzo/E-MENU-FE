715725abda5a62c8ef39273664a1483ec99f603c
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-27 17:20:17 +0800

    chore: add remotePatterns config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.cnn.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
