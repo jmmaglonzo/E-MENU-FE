4535ba26b294cd5546b9e832e60fc78bb3b4359d
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-27 19:12:22 +0800

    feat: add viewOrder Content

--- src/components/Drawer/OrderDrawer.tsx
+++ src/components/Drawer/OrderDrawer.tsx
@@ -1,9 +1,10 @@
+import ViewOrder from "../Order/ViewOrder";
 import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
 
 export const OrderDrawer = () => {
   return (
     <Drawer>
-      <DrawerTrigger className="flex w-full items-center justify-center rounded-md bg-primary text-white">
+      <DrawerTrigger className="inline-flex w-[90%] items-center justify-center rounded-md bg-primary text-white">
         <div className="flex w-full items-center justify-between p-4">
           <div className="flex size-6 w-fit items-center justify-center rounded-full border border-primary-foreground p-2">
             1
@@ -13,7 +14,7 @@ export const OrderDrawer = () => {
         </div>
       </DrawerTrigger>
       <DrawerContent className="container h-full">
-        Insert cart screen
+        <ViewOrder />
       </DrawerContent>
     </Drawer>
   );
