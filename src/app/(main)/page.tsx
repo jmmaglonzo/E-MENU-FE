3c1e1f3d9ea73659f4ebcef82d4f5507f6a22eb2
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-28 11:59:36 +0800

    feat:add OrderDrawer Component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import CardContainer from "@/components/Card/CardContainer";
+import { OrderDrawer } from "@/components/Drawer/OrderDrawer";
 
 export default function Home() {
   return (
     <main className="container">
       <CardContainer />
+      <OrderDrawer />
     </main>
   );
 }
