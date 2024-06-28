6a4b5884761b276436b8e16de0098db546666e7c
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-28 12:02:26 +0800

    style: adjust font size and margin top

--- src/components/Order/TipCard.tsx
+++ src/components/Order/TipCard.tsx
@@ -1,12 +1,12 @@
 import { FaCoins } from "react-icons/fa6";
 const TipCard = () => {
   return (
-    <div className="space-y-2">
-      <span className="flex items-center gap-2 font-semibold">
+    <div>
+      <span className="flex items-center gap-2 text-base font-semibold">
         <FaCoins className="text-primary" />
         Tip your server
       </span>
-      <p className="text-center text-base text-gray-500">
+      <p className="text-sm text-gray-500">
         100% of the tips go to your server, we don’t deduct anything from it.
       </p>
     </div>
