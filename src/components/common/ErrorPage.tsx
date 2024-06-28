1e034dff98ea406dbddf7448eee829fb6d7add59
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-28 12:00:09 +0800

    style: adjust font size and spacing

--- src/components/common/ErrorPage.tsx
+++ src/components/common/ErrorPage.tsx
@@ -1,7 +1,7 @@
 const ErrorPage = () => {
   return (
     <div className="container mt-36 flex min-h-dvh flex-col items-center bg-background px-4 text-center">
-      <h1 className="text-4xl font-bold tracking-tight text-foreground">
+      <h1 className="text-3xl font-bold tracking-tight text-foreground">
         We apologize! Our chef seems to have spilled the secret sauce.
       </h1>
       <p className="mt-4 text-xl text-muted-foreground">
