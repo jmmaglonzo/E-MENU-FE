1a5b44a6b03d7d4bf80ec640e26c435b19419bc4
Kamnida1992 <vincentsanpedro2015@gmail.com>
2024-06-28 12:03:26 +0800

    Fix: Checkout Styles

--- src/components/Order/order_checkout/FooterText.tsx
+++ src/components/Order/order_checkout/FooterText.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const FooterText = () => {
   return (
     <div>
-        <div className='text-[0.7em] flex justify-center items-center leading-4
-        my-10'>
-        <span className='text-center'>
+        <div className=' flex justify-center items-center leading-3
+        '>
+        <span className=' text-gray-800 text-center text-[0.5em]'>
              I hereby give  Chef Morgan Restaurant the permission to share my customer data
           with the Restaurant, and is applicable, their respective affiliates and subsidaries,
           for service improvement and/or other related marketing purposes.
@@ -15,8 +15,8 @@ const FooterText = () => {
           </span>
         </div>
 
-        <div className='text-[0.7em] flex justify-center my-5'>
-        <span className='text-center'>By completing this order, I agree to all
+        <div className='flex justify-center mt-4'>
+        <span className='text-gray-800 text-center text-[0.5em]'>By completing this order, I agree to all
         <Link href="#" className='text-orange-300 font-bold'> terms and conditions</Link></span>
         </div>
     </div>
--- src/components/Order/order_checkout/TotalAmount.tsx
+++ src/components/Order/order_checkout/TotalAmount.tsx
@@ -5,11 +5,11 @@ import React from 'react'
 const TotalAmount = () => {
   return (
     <div className='bg-slate-100 p-2'>
-        <div className='flex flex-col items-center mt-10 gap-y-2'>
+        <div className='flex flex-col items-center mt-8 gap-y-2'>
         <span className='font-bold text-[0.7em]'>Total Amount to Pay</span>
-        <span className='font-bold text-[2em]'>₱100,820.00</span>
-        <span className='text-[0.6em]'>Vat Included <span className='text-orange-300'>learn more?</span></span>
-        <Link href="#" className=' w-[100%] mt-2 rounded-sm bg-primary py-2 text-center font-semibold text-white'>Place Order</Link>
+        <span className='font-bold text-[1.8em]'>₱100,820.00</span>
+        <Link href="#" className='text-[0.6em]'>Vat Included <span className='text-orange-300'>learn more?</span></Link>
+        <Link href="#" className=' w-[100%] mt-2 rounded-sm bg-primary py-2 text-[0.8em] text-center font-semibold text-white'>Place Order</Link>
         </div>
        
     </div>
