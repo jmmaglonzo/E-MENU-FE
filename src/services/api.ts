496c99602e6b792fd16961281c415b354215b683
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 06:51:25 +0800

    feat: get cart items in the be.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import { ItemTypes } from "@/types/productCard";
 import api from "./axios";
+import { CartItem } from "@/types/cart";
 
 export const getProducts = async () => {
   const { data } = await api.get<ItemTypes[]>("products");
   return data;
 };
+
+export const getCartItems = async() => {
+  const { data } = await api.get<CartItem[]>("cart");
+  return data;
+}
