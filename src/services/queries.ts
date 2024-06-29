340ce4b8134a4213ce7f4aaadc33cfa4230bfb7d
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 06:52:32 +0800

    feat: get cart items query.

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,8 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { getProducts } from "./api";
+import { getProducts, getCartItems } from "./api";
 export const useGetProducts = () => {
   return useQuery({
     queryKey: ["products"],
     queryFn: getProducts,
+  });
+};
+
+export const useGetCartItems = () => {
+  return useQuery({
+    queryKey: ["cart"],
+    queryFn: getCartItems,
   });
 };
