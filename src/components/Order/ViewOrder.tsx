c3eede04b4d1902a979cd950e018893cdb21fbc5
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-29 14:18:28 +0800

    feat: display items in cart.

--- src/components/Order/ViewOrder.tsx
+++ src/components/Order/ViewOrder.tsx
@@ -1,19 +1,40 @@
+"use client";
+
 import OrderCards from "@/components/Order/OrderCards";
 import TipCard from "@/components/Order/TipCard";
 import TipSelector from "@/components/Order/TipSelector";
 import TipSummary from "@/components/Order/TipSummary";
-import { fakeOrderData } from "@/utils/orderData";
+import { OrderData } from "@/utils/orderData";
 import Link from "next/link";
 import OrderNav from "./OrderNav";
+import { useCart } from "@/store/cart-store";
+import { useGetProducts } from "@/services/queries";
 
 const ViewOrder = () => {
+  const {items} = useCart();
+  const {data} = useGetProducts();
+
   return (
     <section className="container">
       <OrderNav />
       <div className="flex h-[290px] flex-col gap-2 overflow-y-scroll no-scrollbar">
-        {fakeOrderData.map((items) => (
-          <OrderCards key={items.product_id} data={items} />
-        ))}
+        {
+          items.map(({id,quantity}) => {
+            const item = data?.find(item => item.id == id);
+            
+            if (!item) return <></>;
+
+            const orderItem = {
+               id: item.id,
+               name: item.name,
+               description: item.description,
+               price: item.price,
+               image: item.image
+            } as OrderData;
+
+            return <OrderCards key={item.id} data={orderItem} quantity={quantity} />           
+          })
+        }
       </div>
       <div className="text-center">
         <Link href="/" className="text-base font-semibold text-primary">
