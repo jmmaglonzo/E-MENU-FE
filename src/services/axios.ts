eeeb7160a20f6fc81e621c3031f211f5e8b5d7ef
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 08:57:16 +0800

    update: make the server able to read cookies.

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://e-menu-be.onrender.com/api/",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
