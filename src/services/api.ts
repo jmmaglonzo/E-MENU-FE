c3117ba8592e307a3b5a6e73514f2ee75f42f567
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 09:11:53 +0800

    feat: get cart item from server.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import { ItemTypes } from "@/types/productCard";
 import api from "./axios";
-import { CartItem } from "@/types/cart";
+import { CartItem, CartItemServer } from "@/types/cart";
 
 export const getProducts = async () => {
   const { data } = await api.get<ItemTypes[]>("products");
@@ -8,6 +8,12 @@ export const getProducts = async () => {
 };
 
 export const getCartItems = async() => {
-  const { data } = await api.get<CartItem[]>("cart");
+  const { data } = await api.get<CartItemServer[]>("cart");
+  const items = data.map(item => ({id: item.productId, quantity: item.quantity}));
+  return items;
+}
+
+export const updateCartItems = async(cartItems: CartItem[]) => {
+  const { data } = await api.post("cart/update",{cartItems});
   return data;
 }
