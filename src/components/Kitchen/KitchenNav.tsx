2e6f1e93908af4e91bbc222301dcf386cb8c981a
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-30 13:06:25 +0800

    feat: add kitchen nav component

---
+++ src/components/Kitchen/KitchenNav.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Image from "next/image";
+import menuLogo from "/public/emenu-logo-dark.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const KitchenNav = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="kitchen-container mx-auto flex items-center justify-between">
+      <div className="relative h-[50px] w-[200px]">
+        <Image src={menuLogo} alt="Menu Logo" fill className="object-contain" />
+      </div>
+      <h1 className="mt-2 font-bold">Kitchen Dashboard</h1>
+      <ul className="mt-2 flex flex-1 items-center justify-center gap-4 font-medium">
+        <li>
+          <Link
+            href={"/kitchen"}
+            className={`${pathname === "/kitchen" && "border-b-2 border-primary"}`}
+          >
+            Orders
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/kitchen/assistance"}
+            className={`${pathname === "/kitchen/assistance" && "border-b-2 border-primary"}`}
+          >
+            Assistance
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/kitchen/inventory"}
+            className={`${pathname === "/kitchen/inventory" && "border-b-2 border-primary"}`}
+          >
+            Inventory
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/kitchen/admin"}
+            className={`${pathname === "/kitchen/admin" && "border-b-2 border-primary"}`}
+          >
+            Analytics
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default KitchenNav;
