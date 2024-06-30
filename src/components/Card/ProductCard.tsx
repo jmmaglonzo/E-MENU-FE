e776944efd03ac86679e544a183511bc167022c9
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-06-30 20:12:26 +0800

    feat: add open modal function

--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Toaster, toast } from 'sonner'
+import { Toaster, toast } from "sonner";
 
 import { ItemTypes } from "@/types/productCard";
 import Image from "next/image";
@@ -7,42 +7,44 @@ import { Button } from "../ui/button";
 import { PiClockCountdownFill } from "react-icons/pi";
 import { FaStar } from "react-icons/fa6";
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
-import { useAddCart } from '@/services/queries';
-import { AxiosError } from 'axios';
-
+import { useAddCart } from "@/services/queries";
+import { AxiosError } from "axios";
+import useCardStore from "@/store/productCard-store";
 interface ProductCardProps {
   data: ItemTypes;
 }
 
 const ProductCard = ({ data }: ProductCardProps) => {
-  const { error, isError, mutate} = useAddCart();
-
+  const { error, isError, mutate } = useAddCart();
+  const setSelectedItem = useCardStore((state) => state.setSelectedItem);
   function handleAddCart() {
-     mutate(data.id);
+    mutate(data.id);
   }
 
   if (isError) {
     const errStatus = (error as AxiosError).response?.request.status;
-    
-    if (errStatus === 401)
-      toast("You are not allowed to order!"); 
+
+    if (errStatus === 401) toast("You are not allowed to order!");
   }
 
+  const handleClick = () => {
+    setSelectedItem(data);
+  };
 
   return (
-    <Card className="flex flex-col">
+    <Card className="flex flex-col" onClick={handleClick}>
       <CardHeader className="relative aspect-square h-[120px] w-full overflow-hidden rounded-tl-md rounded-tr-md md:h-[150px]">
         <Image
           fill
           src={data.image}
-          alt="Pastry and Boiled Egg on Plate"
+          alt={data.name}
           className="object-cover"
           sizes="(max-width: 768px) 100vw, 120px"
           priority
         />
       </CardHeader>
       <CardContent className="overflow-hidden p-0 px-3">
-        <Toaster/>
+        <Toaster />
         <h2 className="my-2 truncate text-base font-semibold leading-4">
           {data.name}
         </h2>
