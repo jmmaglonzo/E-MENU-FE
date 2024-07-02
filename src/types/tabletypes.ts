b3304d68008e73dad5b9f59f461c4a31a137afaf
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-02 14:49:38 +0800

    feat: add table types

---
+++ src/types/tabletypes.ts
@@ -0,0 +1,14 @@
+export interface OrderTableTypes {
+  tableNo: number;
+  guests: number;
+  date: string;
+  orderNo: string;
+  status: string;
+  orderList: string[];
+}
+
+export interface TableTypes {
+  tableNo: number;
+  guests: number;
+  date: string;
+}
