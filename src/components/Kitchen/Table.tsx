ec4f96a69b0b7cc4256445f3ad368b0b3ac0c728
joben <jobenapao10@gmail.com>
2024-07-03 19:49:19 +0800

    refactor: Added conditional rendering if table queue is empty

--- src/components/Kitchen/Table.tsx
+++ src/components/Kitchen/Table.tsx
@@ -5,7 +5,18 @@ import { useGetTableQueue } from "@/services/queries";
 
 const Table = () => {
   const { data } = useGetTableQueue();
-  console.log(data);
+
+  if (
+    data?.length === 0 ||
+    data?.filter((table) => table.status === false).length === 0
+  ) {
+    return (
+      <h1 className="mt-10 flex items-center justify-center text-xl text-muted-foreground">
+        No table queues
+      </h1>
+    );
+  }
+
   return (
     <main className="grid grid-cols-4 gap-4">
       {data?.map((table, index) => <TableCards key={index} data={table} />)}
