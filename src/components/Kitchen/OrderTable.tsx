cd2b8d398dd72822371201aadb99967da31d8002
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-05 06:12:53 +0800

    feat: connect data from database.

--- src/components/Kitchen/OrderTable.tsx
+++ src/components/Kitchen/OrderTable.tsx
@@ -1,79 +1,22 @@
-import { OrderTableTypes } from "@/types/table";
+"use client";
+
+import { OrderTableType } from "@/types/table";
 import KitchenOrderCard from "./Cards/KitchenOrderCard";
+import { useGetOrders } from "@/services/queries";
+import OrderModal from "./OrderModal";
 
 const OrderTable = () => {
-  const orders: OrderTableTypes[] = [
-    {
-      tableNo: 1,
-      guests: 4,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-    {
-      tableNo: 2,
-      guests: 3,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-    {
-      tableNo: 3,
-      guests: 2,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-    {
-      tableNo: 4,
-      guests: 4,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-    {
-      tableNo: 5,
-      guests: 2,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-    {
-      tableNo: 6,
-      guests: 3,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-    {
-      tableNo: 7,
-      guests: 2,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-    {
-      tableNo: 8,
-      guests: 4,
-      date: "July 01, 10:30 AM",
-      orderNo: "#1232",
-      status: "Pending",
-      orderList: ["2x Burgers", "1x Salad", "1x Fries"],
-    },
-  ];
+  const { data } = useGetOrders();
+  const orders = data as OrderTableType[] || [];
 
   return (
-    <main className="grid grid-cols-4 gap-4">
-      {orders.map((order) => (
-        <KitchenOrderCard key={order.tableNo} data={order} />
-      ))}
+    <main>
+      <div className="grid grid-cols-4 gap-4">
+        {orders.map((order) => (
+          <KitchenOrderCard key={order.orderNo} data={order} />
+        ))}
+      </div>
+      <OrderModal/>
     </main>
   );
 };
