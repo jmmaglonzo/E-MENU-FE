838085814e7accf104c71117818f27dc9c0d6b2b
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-04 16:41:06 +0800

    style: change date of badge

--- src/components/Kitchen/Cards/TableCards.tsx
+++ src/components/Kitchen/Cards/TableCards.tsx
@@ -18,33 +18,32 @@ const TableCards = ({ data }: TableProps) => {
 
   return (
     //if data.status === false then show the card else do not render
-    !data.status && (
-      <Card className="relative">
-        <CardHeader className="flex items-center justify-between">
-          <CardTitle>Table {data.tableNo}</CardTitle>
-          <div className="flex items-center gap-2">
-            <Badge variant="secondary">{data.guests} guests</Badge>
-            <div className="text-sm text-muted-foreground">{data.date}</div>
-          </div>
-        </CardHeader>
-        <CardContent className="flex justify-between gap-4 font-bold">
-          <Button
-            variant="outline"
-            className="w-full bg-red-600 text-white"
-            onClick={() => handleClick(data.session, "decline")}
-          >
-            Declined
-          </Button>
-          <Button
-            variant="outline"
-            className="w-full bg-green-500 text-white"
-            onClick={() => handleClick(data.session, "approve")}
-          >
-            Approved
-          </Button>
-        </CardContent>
-      </Card>
-    )
+
+    <Card className="relative">
+      <CardHeader className="flex items-center justify-between">
+        <CardTitle>Table {data.tableNo}</CardTitle>
+        <div className="flex items-center gap-2">
+          <Badge variant="secondary">July 04 2024 10:30 AM</Badge>
+          <div className="text-sm text-muted-foreground">{data.date}</div>
+        </div>
+      </CardHeader>
+      <CardContent className="flex justify-between gap-4 font-bold">
+        <Button
+          variant="outline"
+          className="w-full bg-red-600 text-white"
+          onClick={() => handleClick(data.session, "decline")}
+        >
+          Declined
+        </Button>
+        <Button
+          variant="outline"
+          className="w-full bg-green-500 text-white"
+          onClick={() => handleClick(data.session, "approve")}
+        >
+          Approved
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
 
