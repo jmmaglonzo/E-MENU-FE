9877856bcc86b95555efd684994149d459ed9c07
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-06 21:34:11 +0800

    refactor: change from anonymous function to arrow function

--- src/utils/orderTotal.ts
+++ src/utils/orderTotal.ts
@@ -1,16 +1,16 @@
 import { useGetCartItems, useGetProducts } from "@/services/queries";
 
-export default function(): number {
-    const getCartItems = useGetCartItems();
-    const {data} = useGetProducts();
-    
-    const items = getCartItems.data || [];
-  
-    if (items.length === 0) return 0;
-  
-    return items.reduce((amount, cartItem) => {
-      const item = data?.find((item) => item.id == cartItem.id);
-      const price = item?.price || 0;
-      return amount + price * cartItem.quantity;
-    }, 0);
+export default function TotalAmount() {
+  const getCartItems = useGetCartItems();
+  const { data } = useGetProducts();
+
+  const items = getCartItems.data || [];
+
+  if (items.length === 0) return 0;
+
+  return items.reduce((amount, cartItem) => {
+    const item = data?.find((item) => item.id == cartItem.id);
+    const price = item?.price || 0;
+    return amount + price * cartItem.quantity;
+  }, 0);
 }
