a4b1906c3440b6c96e3b5a1f84da2fb92eb24e89
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-06 21:32:06 +0800

    refactor: remove unused components

--- src/components/Order/order_checkout/TotalAmount.tsx
+++ src/components/Order/order_checkout/TotalAmount.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import getOrderTotalAmount from "@/utils/orderTotal";
-import Link from "next/link";
 import React, { MouseEventHandler } from "react";
 
 import { Button } from "@/components/ui/button";
 
 interface TotalAmountProps {
-  handleOrder: Function
+  handleOrder: Function;
 }
 
-const TotalAmount = ({handleOrder}: TotalAmountProps) => {
+const TotalAmount = ({ handleOrder }: TotalAmountProps) => {
   const productAmount = getOrderTotalAmount();
 
   return (
@@ -18,11 +17,11 @@ const TotalAmount = ({handleOrder}: TotalAmountProps) => {
       <div className="h-0.5 w-full bg-slate-100"></div>
       <div className="mt-5 flex flex-col items-center gap-y-2">
         <span className="text-[0.7em] font-bold">Total Amount to Pay</span>
-        <span className="text-[1.8em] font-bold">₱{productAmount.toFixed(2)}</span>
-        <Link href="#" className="text-base">
-          Vat Included <span className="text-primary">Learn more?</span>
-        </Link>
+        <span className="text-[1.8em] font-bold">
+          ₱{productAmount.toFixed(2)}
+        </span>
         <Button
+          type="submit"
           onClick={handleOrder as MouseEventHandler}
           className="mt-2 w-full rounded-sm bg-primary py-2 text-center text-[0.8em] font-semibold text-white"
         >
