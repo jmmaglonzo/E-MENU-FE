16c1ea2d18090ce263a3d87e747b7847387ca3e3
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-07 10:38:32 +0800

    style: change style color base on condition

--- src/components/Order/OrderNav.tsx
+++ src/components/Order/OrderNav.tsx
@@ -8,20 +8,18 @@ const OrderNav = () => {
   return (
     <nav className="container flex flex-col items-center gap-4 px-1.5 py-2 text-center">
       <div className="flex w-full items-center">
-        {pathname === "/checkout" && (
-          <Link href="/">
-            <ChevronLeft size={20} />
-          </Link>
-        )}
+        <Link href="/">
+          <ChevronLeft size={20} />
+        </Link>
         <span className="flex-grow font-bold">
-          {pathname === "/checkout" ? "Order Checkout" : "Order Details"}
+          {pathname === "/checkout" && "Order Checkout"}
         </span>
       </div>
       <div className="flex h-1.5 w-full gap-4">
         <div className="w-1/2 rounded-lg bg-primary"></div>
         <div
           className={`w-1/2 rounded-lg ${
-            pathname === "/checkout" ? "bg-primary" : "bg-gray-300"
+            pathname === "/checkout" && "bg-primary"
           }`}
         ></div>
       </div>
