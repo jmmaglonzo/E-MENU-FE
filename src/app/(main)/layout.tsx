0dc78c93f471f06840bbf5a6754420557a5ce4ac
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-07 19:36:44 +0800

    feat: add loader for initial page loading

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,13 @@
+"use client";
+
+import Loader from "@/components/common/Loader";
 import Navbar from "@/components/header/Navbar";
+import { useGetProducts } from "@/services/queries";
 import { Suspense } from "react";
 import { Toaster } from "sonner";
 
 function NavbarFallback() {
-  return <div>
-    Confirming Registration Failed
-  </div>
+  return <div>Confirming Registration Failed</div>;
 }
 
 export default function MainLayout({
@@ -13,10 +15,19 @@ export default function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { isLoading } = useGetProducts();
+
+  if (isLoading)
+    return (
+      <div className="container mt-72 flex justify-center">
+        <Loader />
+      </div>
+    );
+
   return (
     <>
       <header>
-        <Suspense fallback={<NavbarFallback/>}>
+        <Suspense fallback={<NavbarFallback />}>
           <Navbar />
         </Suspense>
       </header>
