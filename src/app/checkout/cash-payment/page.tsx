b6c84c532c636d783f8586563e7bbf10cb1025aa
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-12 05:05:39 +0800

    feat: updated data.

--- src/app/checkout/cash-payment/page.tsx
+++ src/app/checkout/cash-payment/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { useGetMyLatestOrder } from "@/services/queries";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import QRCode from "react-qr-code";
+import Loader from "@/components/common/Loader";
+import { MyLatestOrder } from "@/types/myOrder";
 
 const CashCheckout = () => {
+  const { data, isSuccess } = useGetMyLatestOrder();
+  const myLatestOrder = data as MyLatestOrder;
   return (
     <section className="container mt-4">
       <nav className="flex items-center gap-2">
@@ -19,34 +26,44 @@ const CashCheckout = () => {
         </p>
       </div>
 
-      <div className="mt-4 flex flex-col space-y-3">
-        <span className="font-bold">Digital Receipt</span>
-        <div className="flex items-center justify-between">
-          <span>Order #</span>
-          <span>1234</span>
-        </div>
-        <div className="flex items-center justify-between">
-          <span>Items</span>
-          <span>3 items</span>
-        </div>
-        <div className="flex items-center justify-between">
-          <span>Total</span>
-          <span>Php 200.00</span>
-        </div>
-      </div>
+      {
+        isSuccess? 
+          <>
+            <div className="mt-4 flex flex-col space-y-3">
+              <span className="font-bold">Digital Receipt</span>
+              <div className="flex items-center justify-between">
+                <span>Order #</span>
+                <span>{data.orderNo}</span>
+              </div>
+              <div className="flex items-center justify-between">
+                <span>Items</span>
+                <span>{data.orders.length} items</span>
+              </div>
+              <div className="flex items-center justify-between">
+                <span>Total</span>
+                <span>Php {data.total.toFixed(2)}</span>
+              </div>
+            </div>
 
-      <div className="mt-8 flex flex-col items-center space-y-8 text-gray-500">
-        <div className="flex justify-center">
-          <QRCode value="https://example.com" size={300} />
-        </div>
-        <span>Show this QR code to the cashier</span>
-      </div>
-      <Link
-        href="/"
-        className="mt-8 inline-block w-full rounded-md bg-primary py-2 text-center text-white"
-      >
-        Go back to Home
-      </Link>
+            <div className="mt-8 flex flex-col items-center space-y-8 text-gray-500">
+              <div className="flex justify-center">
+                <QRCode value="https://example.com" size={300} />
+              </div>
+              <span>Show this QR code to the cashier</span>
+            </div>
+            <Link
+              href="/"
+              className="mt-8 inline-block w-full rounded-md bg-primary py-2 text-center text-white"
+            >
+              Go back to Home
+            </Link>
+          </>
+          :
+          <div className="container flex justify-center mt-64">
+              <Loader />
+          </div> 
+      }
+      
     </section>
   );
 };
