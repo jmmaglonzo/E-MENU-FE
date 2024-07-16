8951c85606b2187958744df0de6704620a18c168
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-16 08:09:59 +0800

    refactor: removed socket types.

--- src/types/socket.ts
+++
@@ -1,7 +0,0 @@
-import { Socket } from "socket.io-client";
-import { OrderStatus } from "./myOrder";
-
-export interface SocketEvents {
-    updateStatus: (orderNo: number, status: OrderStatus) => void;
-    socket: Socket | null;
-}
