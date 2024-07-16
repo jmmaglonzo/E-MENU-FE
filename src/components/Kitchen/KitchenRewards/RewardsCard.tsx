dadcec181f8fd92fd56f47fef2cfca856b04d0ea
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-16 20:36:09 +0800

    feat: add reward card component

---
+++ src/components/Kitchen/KitchenRewards/RewardsCard.tsx
@@ -0,0 +1,42 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import RewardEditModal from "./RewardEditModal";
+import KitchenDeleteModal from "../KitchenModal/KitchenDeleteModal";
+interface RewardCardProps {
+  item: {
+    name: string;
+    description: string;
+    points: number;
+  };
+}
+const RewardsCard = ({
+  item: { name, description, points },
+}: RewardCardProps) => {
+  return (
+    <Card className="flex flex-col space-y-2 rounded-sm border p-4 shadow-sm">
+      <CardHeader className="p-0">
+        <CardTitle>{name}</CardTitle>
+      </CardHeader>
+      <CardContent className="p-0">
+        <CardDescription className="mb-2">{description}</CardDescription>
+
+        <CardFooter className="flex items-center justify-between p-0">
+          <span>{points} points</span>
+
+          <div className="flex items-center gap-2">
+            <RewardEditModal />
+            <KitchenDeleteModal />
+          </div>
+        </CardFooter>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default RewardsCard;
