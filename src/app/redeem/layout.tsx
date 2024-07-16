c05e86123d266f1f1aae5178d898b22def87e088
joben <jobenapao10@gmail.com>
2024-07-16 15:06:54 +0800

    fix: hydration error

--- src/app/redeem/layout.tsx
+++ src/app/redeem/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
+import ClientOnly from "@/components/common/ClientOnly";
 import { getCookie } from "cookies-next";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 export default function MainLayout({
   children,
@@ -12,7 +13,7 @@ export default function MainLayout({
   const router = useRouter();
   const cookie = getCookie("_loyalty_session");
   const [session, setSession] = useState<string>();
-  let linkRoute = cookie ? "/" : "/redeem";
+  const linkRoute = "/";
   const headerText = cookie ? "Available Points" : "Loyalty Points";
 
   useEffect(() => {
@@ -22,14 +23,16 @@ export default function MainLayout({
 
   return (
     <>
-      <header className="mobile-container relative" suppressHydrationWarning>
-        <div className="flex flex-col items-center justify-between space-y-4 p-0 py-2 text-center">
-          <Link href={linkRoute} className="absolute left-0 top-6">
-            <ChevronLeft className="h-7 w-7 text-muted-foreground" />
-          </Link>
-          <span className="text-2xl font-bold">{headerText}</span>
-        </div>
-      </header>
+      <ClientOnly>
+        <header className="mobile-container relative" suppressHydrationWarning>
+          <div className="flex flex-col items-center justify-between space-y-4 p-0 py-2 text-center">
+            <Link href={linkRoute} className="absolute left-0 top-6">
+              <ChevronLeft className="h-7 w-7 text-muted-foreground" />
+            </Link>
+            <span className="text-2xl font-bold">{headerText}</span>
+          </div>
+        </header>
+      </ClientOnly>
       <main className="mobile-container" suppressHydrationWarning>
         {children}
       </main>
