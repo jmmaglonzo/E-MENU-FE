416342c3d02f5f7a0f686b565ec4d956df3a768a
joben <jobenapao10@gmail.com>
2024-07-18 13:13:11 +0800

    fix: hydration error

--- src/app/kitchen/layout.tsx
+++ src/app/kitchen/layout.tsx
@@ -1,3 +1,4 @@
+import ClientOnly from "@/components/common/ClientOnly";
 import KitchenNav from "@/components/Kitchen/KitchenNav";
 
 export default function MainLayout({
@@ -8,7 +9,10 @@ export default function MainLayout({
   return (
     <>
       <header className="my-2">
-        <KitchenNav />
+        <ClientOnly>
+          {" "}
+          <KitchenNav />
+        </ClientOnly>
       </header>
       <main className="container mx-auto mt-6 flex-1">{children}</main>
     </>
