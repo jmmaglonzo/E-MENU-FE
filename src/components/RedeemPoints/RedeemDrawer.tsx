e9deab7d4161e455ec3690583cb6e3175ebf2f50
joben <jobenapao10@gmail.com>
2024-07-18 16:01:47 +0800

    feat: rewards list

--- src/components/RedeemPoints/RedeemDrawer.tsx
+++ src/components/RedeemPoints/RedeemDrawer.tsx
@@ -13,8 +13,12 @@ import {
 } from "../ui/drawer";
 import { cardRedeem } from "@/utils/cardRedeem";
 import RedeemCard from "./RedeemCard";
+import { useGetRewardsList } from "@/services/queries";
+import { ScrollArea } from "../ui/scroll-area";
 
 const RedeemDrawer = () => {
+  const { data: rewards } = useGetRewardsList();
+  console.log("rewards", rewards);
   return (
     <Drawer>
       <DrawerTrigger className="rounded-sm bg-primary px-6 py-1 text-sm font-medium text-white">
@@ -30,13 +34,25 @@ const RedeemDrawer = () => {
             food-related items.
           </DrawerDescription>
         </DrawerHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-2 gap-4">
-            {cardRedeem.map((item) => (
-              <RedeemCard key={item.title} data={item} />
-            ))}
+        {rewards !== undefined && rewards?.length >= 1 && (
+          <ScrollArea className="size-full">
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-2 gap-4">
+                {rewards.map((item) => (
+                  <RedeemCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
+          </ScrollArea>
+        )}
+
+        {rewards !== undefined && rewards?.length < 1 && (
+          <div className="flex h-[400px] items-center justify-center">
+            <span className="text-base text-muted-foreground">
+              No rewards available
+            </span>
           </div>
-        </div>
+        )}
         <DrawerFooter>
           <DrawerClose asChild>
             <Button variant="outline">Close</Button>
